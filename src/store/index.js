afaf0933696aea48f3ce2d6617a15344c7b3abf5
ayhamhashesh <ayham.hashesh@gmail.com>
2021-10-06 15:24:25 +0300

    Use getSlice

--- src/store/index.js
+++ src/store/index.js
@@ -1,34 +1,34 @@
-// Import Redux
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
 
-// Reducer functions
-const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
-    return { counter: state.counter + 1, showCounter: state.showCounter };
-  }
-  if (action.type === "decrement") {
-    return { counter: state.counter - 1, showCounter: state.showCounter };
-  }
-  if (action.type === "increase") {
-    return {
-      counter: state.counter + action.amount,
-      showCounter: state.showCounter,
-    };
-  }
-  if (action.type === "toggle") {
-    return {
-      counter: state.counter,
-      showCounter: !state.showCounter,
-    };
-  }
+// Slice of our global states
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialState,
+  reducers: {
+    // By default recieve the latest state
+    increment(state) {
+      // mutate the state
+      state.counter++;
+      // toolkit packet will update the state automatically
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  return state;
-};
 
-// Create Store
-const store = createStore(counterReducer);
-
+const store = configureStore({
+  reducer: counterSlice.reducer,
+});
 // Provide store to react
 export default store;
+export const counterActions = counterSlice.actions;
